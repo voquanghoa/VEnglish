a9ddcefc1447c6f03d2fadfb8e588d699f61c52c
Vo Quang Hoa <voquanghoa@gmail.com>
2015-12-20 21:01:34 +0700

    Add assert data + init tool project

---
+++ app/src/main/java/com/qhvv/englishforalllevel/model/DataItem.java
@@ -0,0 +1,35 @@
+package com.qhvv.englishforalllevel.model;
+
+import java.util.List;
+
+
+public class DataItem {
+    private String fileName;
+    private String display;
+    private List<DataItem> children;
+
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(String display) {
+        this.display = display;
+    }
+
+    public List<DataItem> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<DataItem> children) {
+        this.children = children;
+    }
+}
